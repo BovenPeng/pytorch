b0cee66b315dbe0b69c2c6e5613f744f33c2ba18
Joel Schlosser <jbschlosser@meta.com>
2024-07-17 18:51:50 -0400

    Avoid graph break during NJT construction
    ghstack-source-id: cfe3272dc806a08042ff5afec81966a1d2e4a09e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130505

--- torch/_functorch/_aot_autograd/collect_metadata_analysis.py
+++ torch/_functorch/_aot_autograd/collect_metadata_analysis.py
@@ -157,7 +157,13 @@ def run_functionalized_fw_and_collect_metadata(
         # It doesn't matter if we run this under predispatch or not because it is
         # only for figuring out metadata
         mode = FunctionalTensorMode(_allow_token_discovery=True)
-        with disable_above, mode:
+
+        # NB: emulate idempotence during metadata collection by preventing the nested
+        # int ID counter from advancing.
+        # TODO: Determine how to use branch_nested_int_registry() instead!
+        from torch.nested._internal.nested_tensor import freeze_nested_int_counter
+
+        with disable_above, mode, freeze_nested_int_counter():
             # precondition: The passed in function already handles unflattening inputs + flattening outputs
             flat_f_args = pytree.tree_map(_to_fun, flat_args)
             flat_f_outs = f(*flat_f_args)
@@ -628,7 +634,16 @@ from a multi-output view call"
                     t, lambda _, inner_t: view_avoid_dupes_with_primals(inner_t)
                 )
             if isinstance(t, Tensor):
-                return t.view(t.shape)
+                out = t.view(t.shape)
+
+                # indicate that the viewed tensor has the same associated nested int
+                # as the source tensor within the nested int registry
+                from torch.nested._internal.nested_tensor import _nested_int_registry
+
+                if t in _nested_int_registry:
+                    _nested_int_registry.set(out, _nested_int_registry.get(t))
+
+                return out
             return t
 
         # This analysis function returns *only* the outputs that are meant to be tangents to the backwards.
