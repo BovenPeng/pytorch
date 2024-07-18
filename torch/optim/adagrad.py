6802c78f7ecac71458ee9381b07220713533f4c1
BovenPeng <43337427+BovenPeng@users.noreply.github.com>
2024-07-18 19:04:26 +0800

    comment: add comments on adagrad.py

--- torch/optim/adagrad.py
+++ torch/optim/adagrad.py
@@ -80,9 +80,13 @@ class Adagrad(Optimizer):
             if foreach:
                 raise RuntimeError("`fused` and `foreach` cannot be `True` together.")
 
-        for group in self.param_groups:
+        # Q. 怎么理解存的state
+        # A. sum是历史梯度的平方和
+         in self.param_groups:
             for p in group["params"]:
                 state = self.state[p]
+
+                # 某一版实现是 `step = torch.tensor(0.0)`, 用浮点数保持类型一致
                 state["step"] = (
                     torch.zeros(
                         (),
@@ -94,9 +98,13 @@ class Adagrad(Optimizer):
                 )
                 init_value = (
                     complex(initial_accumulator_value, initial_accumulator_value)
+                    # Q. API torch.is_complex
+                    # A. p是否为复数, 常用于信号(声音和图像)
                     if torch.is_complex(p)
                     else initial_accumulator_value
                 )
+                # Q. API torch.full_like, torch.preserve_format
+                # A. 创建一个和指定 Tensor 的形状 (shape)，数据类型 (dtype) 以及硬件设备 (device, 如 CPU 或 GPU) 相同的新 Tensor，并且新 Tensor 的每个元素都被设置为指定的值; memory_format指内存格式(sparse, contiguous)
                 state["sum"] = torch.full_like(
                     p, init_value, memory_format=torch.preserve_format
                 )
@@ -106,13 +114,16 @@ class Adagrad(Optimizer):
         #  define "fused" for
         #  MYPY error: Name "fused" may be undefined
         fused = None
-        for group in self.param_groups:
+         in self.param_groups:
             group.setdefault("foreach", None)
             group.setdefault("maximize", False)
             group.setdefault("differentiable", False)
             fused = group.setdefault("fused", None)
-
+        
+        # 确保 "step" 键的值是 Tensor 类型。如果不是 Tensor 类型，它将在后面部分代码中被转化为 Tensor
         state_values = list(self.state.values())
+        # Q. API torch.is_tensor()
+        # A. 用于检查某个对象是否是 PyTorch Tensor 对象。
         step_is_tensor = (len(state_values) != 0) and torch.is_tensor(
             state_values[0]["step"]
         )
@@ -121,17 +132,21 @@ class Adagrad(Optimizer):
                 s["step"] = torch.tensor(
                     float(s["step"]), dtype=_get_scalar_dtype(is_fused=fused)
                 )
-
+    # 用于在多个进程之间共享模型参数，这在多进程训练时非常有用。开启多进程训练可以更好地利用硬件资源并加速训练过程。由于每个进程都有自己的内存空间，所以默认情况下，各进程中的变量值不共享
     def share_memory(self):
-        for group in self.param_groups:
+         in self.param_groups:
             for p in group["params"]:
                 state = self.state[p]
+                # Q. API .share_memory_()
+                # A. Tensor 提供了 share_memory_() 这个方法，用于共享在不同进程之间的内存。
                 state["sum"].share_memory_()
 
     def _init_group(self, group, params_with_grad, grads, state_sums, state_steps):
         has_sparse_grad, has_complex = False, False
         for p in group["params"]:
             if p.grad is not None:
+                # Q. API p.grad.is_sparse
+                # A. 检查参数 p 的梯度是否是稀疏的。如果 p 的梯度是稀疏的，那么 has_sparse_grad 就被设置为 True。然后，将参数 p，它的梯度，以及和 p 相关联的状态加入到相应的列表中。在后面的优化步骤中，这些列表将会被用于更新 p 的值。
                 has_sparse_grad |= p.grad.is_sparse
                 has_complex |= torch.is_complex(p)
                 params_with_grad.append(p)
@@ -142,6 +157,8 @@ class Adagrad(Optimizer):
 
         return has_sparse_grad, has_complex
 
+    # Q. @_use_grad_for_differentiable
+    # A. @_use_grad_for_differentiable 装饰器可能用于确保 step 函数只更新那些可微分的参数。
     @_use_grad_for_differentiable
     def step(self, closure=None):
         """Perform a single optimization step.
@@ -153,10 +170,11 @@ class Adagrad(Optimizer):
         loss = None
 
         if closure is not None:
+            # Q. API param `use_fused`, data is on device or host
             with torch.enable_grad():
                 loss = closure()
 
-        for group in self.param_groups:
+         in self.param_groups:
             params_with_grad: List[Tensor] = []
             grads: List[Tensor] = []
             state_sums: List[Tensor] = []
@@ -291,6 +309,8 @@ def adagrad(
 
     if fused and not torch.jit.is_scripting():
         func = _fused_adagrad
+    # Q. 为什么要分 multi和single tensor adagrad呢?
+    # A. 1. 处理多个/单个tensor; 2. 并行计算是否开启(参数foreach); 3. 是否在TorchScript模式下运行
     elif foreach and not torch.jit.is_scripting():
         func = _multi_tensor_adagrad
     else:
@@ -315,6 +335,26 @@ def adagrad(
 
 
 def _make_sparse(grad, grad_indices, values):
+    """
+    # 稀疏 tensor 的 indices
+    indices = torch.tensor([[0, 1, 1],
+                            [2, 0, 2]])
+
+    # 稀疏 tensor 的 values
+    values = torch.tensor([3, 4, 5])
+
+    # 用 indices 和 values 构造 grad
+    grad = torch.sparse_coo_tensor(indices, values, [2, 4])
+
+    # 使用函数
+    sparse_tensor = _make_sparse(grad, indices, values)
+
+    # 打印输出
+    print(sparse_tensor.to_dense())
+
+    tensor([[0, 0, 3, 0],
+        [4, 0, 5, 0]])
+    """
     size = grad.size()
     return torch.sparse_coo_tensor(grad_indices, values, size)
 
@@ -340,6 +380,10 @@ def _single_tensor_adagrad(
     for param, grad, state_sum, step_t in zip(params, grads, state_sums, state_steps):
         # update step
         step_t += 1
+        # Q. 为什么 _get_value(step_t) ?
+        # A. 使用 _get_value 函数从 step_t（这是一个 Tensor 对象）中取出值，并将它赋值给 step
+        #   x.item() 返回一个标准 Python 数字，这是一个标量值
+        #   x 返回一个包含该值的 Tensor
         step = _get_value(step_t)
         grad = grad if not maximize else -grad
 
@@ -348,16 +392,21 @@ def _single_tensor_adagrad(
                 raise RuntimeError(
                     "weight_decay option is not compatible with sparse gradients"
                 )
+            # grad += param * weight_decay
             grad = grad.add(param, alpha=weight_decay)
 
+        # (step - 1) * lr_decay作为decay
         clr = lr / (1 + (step - 1) * lr_decay)
 
         if grad.is_sparse:
+            # Q. API coalesce(), _indices(), _values(), _make_sparse(), sparse_mask()
             grad = grad.coalesce()  # the update is non-linear so indices must be unique
             grad_indices = grad._indices()
             grad_values = grad._values()
 
+            # _make_sparse() 根据这些索引和平方值更新历史平方梯度和
             state_sum.add_(_make_sparse(grad, grad_indices, grad_values.pow(2)))
+            # 获取 state_sum 在 grad 非零元素位置的值，计算出每个位置的 std 值（即历史平方梯度和的平方根）
             std = state_sum.sparse_mask(grad)
             std_values = std._values().sqrt_().add_(eps)
             param.add_(
@@ -365,16 +414,25 @@ def _single_tensor_adagrad(
             )
         else:
             is_complex = torch.is_complex(param)
+
+            # switch complex num 2 real num
             if is_complex:
                 grad = torch.view_as_real(grad)
                 state_sum = torch.view_as_real(state_sum)
                 param = torch.view_as_real(param)
+            # Q. API addcmul_
+            # element-wise相乘并且就地累加: state_sum += value * (grad * grad)
             state_sum.addcmul_(grad, grad, value=1)
+            # 可微分 <=> 是否要反向传播
             if differentiable:
                 std = state_sum.sqrt() + eps
             else:
                 std = state_sum.sqrt().add_(eps)
+
+            # Q. API addcdiv_(), view_as_complex()
+            # element-wise相除并且地累加: param += (-clr) * (grad / std)
             param.addcdiv_(grad, std, value=-clr)
+            # swith real num back complex num
             if is_complex:
                 param = torch.view_as_complex(param)
                 state_sum = torch.view_as_complex(state_sum)
@@ -403,16 +461,21 @@ def _multi_tensor_adagrad(
     # Foreach functions will throw errors if given empty lists
     if len(params) == 0:
         return
-
+        
+    # 将 参数params, 梯度grads, 历史梯度平方和state_sums, 步数steps 按照device和dtype来划分; 因为数据是 GPU and CPU fused
     grouped_tensorlists = Optimizer._group_tensors_by_device_and_dtype(
         [params, grads, state_sums, state_steps]
     )
+
+    # 将同一device, dtype的tensor进行并行计算
     for (
         device_params,
         device_grads,
         device_state_sums,
         device_state_steps,
     ), _ in grouped_tensorlists.values():
+
+        # 同一组内只要有一个参数的梯度为sparse, 则调用_single_tensor_adagrad来进行优化
         device_has_sparse_grad = has_sparse_grad and any(
             grad.is_sparse for grad in device_grads
         )
@@ -440,6 +503,7 @@ def _multi_tensor_adagrad(
         if has_complex:
             _view_as_real(device_params, device_grads, device_state_sums)
 
+        # 优化目标最大化 <=> 优化目标最小化 和 梯度相反
         if maximize:
             device_grads = torch._foreach_neg(device_grads)  # type: ignore[assignment]
 
@@ -457,6 +521,7 @@ def _multi_tensor_adagrad(
         if weight_decay != 0:
             # Re-use the intermediate memory (device_grads) already allocated for maximize
             if maximize:
+                # device_grads = device_grad + device_param * alpha
                 torch._foreach_add_(device_grads, device_params, alpha=weight_decay)
             else:
                 device_grads = torch._foreach_add(  # type: ignore[assignment]
@@ -467,9 +532,12 @@ def _multi_tensor_adagrad(
             -lr / (1 + (_get_value(step) - 1) * lr_decay) for step in device_state_steps
         ]
 
+        # each ele, device_state_sums += value * (device_grads * device_grads)
         torch._foreach_addcmul_(device_state_sums, device_grads, device_grads, value=1)
 
+        # each ele, device_state_sums = sqrt(device_state_sums)
         std = torch._foreach_sqrt(device_state_sums)
+        # each ele, std += eps
         torch._foreach_add_(std, eps)
 
         if weight_decay != 0 or maximize:
@@ -479,6 +547,7 @@ def _multi_tensor_adagrad(
         else:
             numerator = torch._foreach_mul(device_grads, minus_clr)  # type: ignore[assignment]
 
+        # device_params += 1 * std/ numerator
         torch._foreach_addcdiv_(device_params, numerator, std)
 
 
